fda7e9661c14bfdab7a90a0cb0888d20e2eddc3e
Matheus Marchi Moro <71672437+MarchiMM@users.noreply.github.com>
2022-06-22 17:50:31 -0300

    Alterações

--- exercises/list06/MMM-Alg-06-Ex-12.py
+++ exercises/list06/MMM-Alg-06-Ex-12.py
@@ -1,4 +1,5 @@
 # Verifica se está em ordem crescente
+# Ocorre verificando os elementos de um em um
 # def is_ordered(list):
 #     for i in range(len(list)-1):
 #         if list[i] > list[i+1]:
@@ -8,17 +9,22 @@
 def input_to_int_list():
     int_list = []
     while True:
-        int_input = int(input("Informe um número inteiro (zero para parar): "))
-        if int_input == 0:
+        str_input = input("Informe um número inteiro (Enter para parar): ")
+        if str_input == "":
             break
+
+        int_input = int(str_input)
 
         int_list.append(int_input)
 
     return int_list
 
 def is_ordered(list):
-    sorted_aux_list = list.sort()
-    sorted_reverse_aux_list = list.sort(reverse=True)
+    if len(list) == 0 or len(list) == 1:
+        return True
+
+    sorted_aux_list = sorted(list)
+    sorted_reverse_aux_list = sorted(list, reverse=True)
 
     if list == sorted_aux_list or list == sorted_reverse_aux_list:
         return True
---
+++ exercises/list06/MMM-Alg-06-Ex-13.py
@@ -0,0 +1,15 @@
+def countRange(list, min_value, max_value):
+    result = 0
+
+    i = 0
+    while i < len(list):
+        if list[i] >= min_value and list[i] < max_value:
+            result += 1
+
+    return result
+
+def main():
+    list = [1, 1, 2, 3, 5, 7, 8, 9, 25, 30, 35]
+
+if __name__ == "__main__":
+    main()
