f180fc799d72d01f132f3ae9c7d3210931401543
Matheus Marchi Moro <71672437+MarchiMM@users.noreply.github.com>
2022-06-27 13:47:05 -0300

    Alterações

--- exercises/list06/MMM-Alg-06-Ex-15.py
+++ exercises/list06/MMM-Alg-06-Ex-15.py
@@ -1,6 +1,3 @@
-from string import punctuation
-
-
 def precedence(operator: str):
     operator_list = ['+-', '*/', '^']
 
@@ -14,57 +11,22 @@ def precedence(operator: str):
 def expression_to_token_list(expression: str):
     token_list = []
     token = ''
-    punctuation_list = ['(', ')']
-    multiplicative_operator_list = ['*', '/', '^']
+    expression = expression.replace(' ', '')
+
     additive_operator_list = ['+', '-']
-    diverse_list = ['.', ',']
-    add_flag = False
+    diverse_list = ['*', '/', '^', '(' , ')']
 
     i = 0
     while i < len(expression):
         char = expression[i]
-        if i > 0:
-            previous_char = expression[i - 1]
-        if i < len(expression) - 1:
-            next_char = expression[i + 1]
 
-        if char == ' ':
-            i += 1
-            continue
-        elif char in (multiplicative_operator_list + punctuation_list):
-            token = char
-            add_flag = True
-        elif char in additive_operator_list:
-            token = char
-            if previous_char.isdigit() or previous_char == ')':
-                add_flag = True
-            else:
-                add_flag = False
-        elif char in diverse_list or char.isdigit():
-            token += char
-
-        if add_flag:
-            token_list.append(token)
-            token = ''
-            add_flag = False
-
-        if next_char in (punctuation_list + multiplicative_operator_list + additive_operator_list + diverse_list) or next_char == ' ':
-            token_list.append(token)
-            token = ''
-
+        if char in additive_operator_list:
+            
         i += 1
-
-    if token != '':
-        token_list.append(token)
-
     return token_list
 
-
-
 def main():
-    expression = '(1+2)*3^(-4/5)'
-    token_list = expression_to_token_list(expression)
-    print(token_list)
+    pass
 
 if __name__ == "__main__":
     main()
