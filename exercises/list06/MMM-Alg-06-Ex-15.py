5f40a493054bf7b2790870c3479eef199e0f2038
Matheus Marchi Moro <71672437+MarchiMM@users.noreply.github.com>
2022-06-28 15:48:32 -0300

    Alterações

--- exercises/list06/MMM-Alg-06-Ex-15.py
+++ exercises/list06/MMM-Alg-06-Ex-15.py
@@ -1,53 +1,40 @@
-def precedence(operator: str):
-    operator_list = ['+-', '*/', '^']
-
-    for element in operator_list:
-        for char in element:
-            if operator == char:
-                return operator_list.index(element) + 1
-
-    return -1
-
 def expression_to_token_list(expression: str):
     expression = expression.replace(' ', '')
     token_list = []
     additive_operator_list = ['+', '-']
 
     token = expression[0]
+
     i = 1
     while i < len(expression):
         char = expression[i]
-
-        def append_token(append_char: bool):
-            if append_char:
-                token_list.append(token)
-                token = ''
-                token_list.append(char)
-            else:
-                token_list.append(token)
-                token = ''
 
         if char.isdigit():
             token += char
         elif char in additive_operator_list:
             previous_char = expression[i - 1]
             if previous_char == ')' or previous_char.isdigit() or previous_char == '':
-                append_token(False)
+                token_list.append(token) if token != '' else None
+                token = ''
                 token += char
             else:
-                append_token(True)
+                token_list.append(token) if token != '' else None
+                token = ''
+                token_list.append(char)
         else:
-            append_token(True)
+            token_list.append(token) if token != '' else None
+            token = ''
+            token_list.append(char)
 
         i += 1
     else:
-        token_list.append(token)
+        token_list.append(token) if token != '' else None
 
 
     return token_list
 
 def main():
-    expression = '1 + 2 * 3 - 4 ^ (-4 * x)'
+    expression = '1 + 2 * 3 - 4 ^ (4 * (x - 2))'
     token_list = expression_to_token_list(expression)
     print(token_list)
 
