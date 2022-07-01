5d3484f4885dd3d028975653506a481f958ececf
Matheus Marchi Moro <71672437+MarchiMM@users.noreply.github.com>
2022-07-01 11:31:02 -0300

    Alterações

---
+++ exercises/list08/MMM-Alg-08-Ex-08.py
@@ -0,0 +1,12 @@
+def rec_dec_to_bin(n: int):
+    pre_converted = {0: "0", 1: "1"}
+    if n in pre_converted:
+        return pre_converted[n]
+    
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
