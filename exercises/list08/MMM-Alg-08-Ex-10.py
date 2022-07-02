ff2a4fa6c42ac44e98994ab681c076e25ab04741
Matheus Marchi Moro <71672437+MarchiMM@users.noreply.github.com>
2022-07-02 18:45:58 -0300

    Alterações

--- exercises/list08/MMM-Alg-08-Ex-10.py
+++ exercises/list08/MMM-Alg-08-Ex-10.py
@@ -10,10 +10,24 @@ def run_length_decode(list: list):
 
     return result
 
+def rec_run_length_decode(list: list):
+    if len(list) == 0:
+        return []
+    if len(list) == 1:
+        return [list[0]]
+
+    result = []
+
+    for j in range(list[1]):
+            result.append(list[0])
+
+    return result + rec_run_length_decode(list[2:])
+
 
 def main():
     list = ["A", 12, "B", 4, "A", 6, "B", 1]
-    print(run_length_decode(list))
+    # print(run_length_decode(list))
+    print(rec_run_length_decode(list))
 
 if __name__ == "__main__":
     main()
